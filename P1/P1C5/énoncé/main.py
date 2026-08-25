e7c17444fdd552794ac1579fd0d0cfe8c9f30214
printer-2 <princedibi0713@gmail.com>
2026-08-26 01:44:02 +0200

    Add variables and print statements in main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+nom = "OKOU"
+age = 24
+taille = 1.76
+est_etudiant = True
+print(nom)
+print(age)
+print(taille)
+print(est_etudiant)
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
