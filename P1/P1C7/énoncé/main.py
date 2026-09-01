1e5db3cafb3b2a30a529a444c1aba511d7f75132
printer-2 <princedibi0713@gmail.com>
2026-09-02 00:40:30 +0200

    Update fruits dictionary with new entries and modifications
    Modify the fruits dictionary by changing the color of 'pomme', adding 'kiwi', and removing 'banane'.

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,5 @@
-# Écrivez votre code ici !
+fruits = {"pomme": "rouge", "banane": "jaune", "orange": "orange"}
+fruits["kiwi"] = "vert"
+fruits["pomme"] = "vert"
+del fruits["banane"]
+print(fruits)
